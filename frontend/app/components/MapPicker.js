e12b4b533fe1f500b05ae5fc4a23ab13f29386fa
NHK-INSANE <rohanpal99038@gmail.com>
2026-04-23 08:46:55 +0530

    UI: Fixed missing Leaflet icons in MapPicker component

--- frontend/app/components/MapPicker.js
+++ frontend/app/components/MapPicker.js
@@ -2,6 +2,16 @@
 
 import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
 import { useState } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+// Fix Leaflet Icon Bug
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
 
 function ClickHandler({ setLocation, setAddress, initialLocation }) {
   const [pos, setPos] = useState(initialLocation ? [initialLocation.lat, initialLocation.lng] : null);
